6818945686c214d841be2cb19c1df43318194380
ogasawara-ryou <ogasawara.ryou@tokyo-gas.co.jp>
2025-10-12 08:39:37 +0900

    再作成

---
+++ base/views/item_views.py
@@ -0,0 +1,56 @@
+from django.views.generic import ListView, DetailView
+from base.models import Item, Category, Tag
+from django.views.generic.edit import CreateView, UpdateView
+
+
+class IndexListView(ListView):
+    model = Item
+    template_name = 'pages/index.html'
+    queryset = Item.objects.filter(is_published=True)
+    paginate_by = 10 #１ページに表示するアイテムの数
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'pages/item.html'
+
+
+class CategoryListView(ListView):
+    model = Item
+    template_name = 'pages/list.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        self.category = Category.objects.get(slug=self.kwargs['pk'])
+        return Item.objects.filter(
+            is_published=True, category=self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = f"Category #{self.category.name}"
+        return context
+
+
+class TagListView(ListView):
+    model = Item
+    template_name = 'pages/list.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        self.tag = Tag.objects.get(slug=self.kwargs['pk'])
+        return Item.objects.filter(is_published=True, tags=self.tag)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = f"Tag #{self.tag.name}"
+        return context
+
+class ItemCreateView(CreateView): #新規作成
+    model = Item
+    template_name = 'snippets/item_form.html'
+    fields = '__all__'
+    
+class ItemUpdateView(UpdateView):
+    model = Item
+    fields = '__all__'
+    template_name_suffix = '_update_form'
---
+++ config/urls.py
@@ -0,0 +1,54 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from base import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    # Account
+    path('admin/', admin.site.urls),
+    path('login/', views.Login.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('signup/', views.SignUpView.as_view()),
+    path('account/', views.AccountUpdateView.as_view()),
+    path('profile/', views.ProfileUpdateView.as_view()),
+
+    # Order
+    path('orders/<str:pk>/', views.OrderDetailView.as_view()),
+    path('orders/', views.OrderIndexView.as_view()),
+
+    # Pay
+    path('pay/checkout/', views.PayWithStripe.as_view()),
+    path('pay/success/', views.PaySuccessView.as_view()),
+    path('pay/cancel/', views.PayCancelView.as_view()),
+
+    # Cart
+    path('cart/add/', views.AddCartView.as_view()),
+    path('cart/remove/<str:pk>/', views.remove_from_cart),
+    path('cart/', views.CartListView.as_view()),
+
+    # Items
+    #path('item/{{object.pk}}/', views.ItemDetailView.as_view()),
+    path('items/<str:pk>/', views.ItemDetailView.as_view(), name="item_detail"),
+    path('categories/<str:pk>/', views.CategoryListView.as_view()),
+    path('tags/<str:pk>/', views.TagListView.as_view()),
+    path('new/', views.ItemCreateView.as_view(), name="new"),
+    path('edit/<str:pk>/', views.ItemUpdateView.as_view(), name="edit"),
+
+    path('', views.IndexListView.as_view(), name="list"),
+]
